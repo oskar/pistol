7627c0fdafe8dce3b61d35e918f590aba690fc3e
Oskar Hermansson <oskar.hermansson@gmail.com>
2014-09-24 17:35:33 +0200

    Renamed IGameEngine to IBangStrategy

---
+++ Pistol.NET/Pistol.NET.Tests/RandomBangStrategyFixture.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+
+namespace Pistol.NET.Tests
+{
+  using System.Collections.Generic;
+
+  [TestFixture]
+  public class RandomBangStrategyFixture
+  {
+    [Test]
+    public void TestBangOneOnTwo_NeverReturnsNone()
+    {
+      // Arrange
+      var bangStrategy = new RandomBangStrategy();
+      var victimGuns = new List<Gun>();
+
+      // Act
+      for (int i = 0; i < 100; i++)
+      {
+        victimGuns.Add(bangStrategy.BangOneOnTwo(1, 1, 1));
+      }
+
+      // Assert
+      Assert.That(victimGuns, Is.All.Not.EqualTo(Gun.None));
+    }
+
+    [Test]
+    public void TestBangTwoOnOne_NeverReturnsNone()
+    {
+      // Arrange
+      var bangStrategy = new RandomBangStrategy();
+      var shooterGuns = new List<Gun>();
+
+      // Act
+      for (int i = 0; i < 100; i++)
+      {
+        shooterGuns.Add(bangStrategy.BangTwoOnOne(1, 1, 1));
+      }
+
+      // Assert
+      Assert.That(shooterGuns, Is.All.Not.EqualTo(Gun.None));
+    }
+
+    [Test]
+    public void TestBang_NeverReturnsNone()
+    {
+      // Arrange
+      var bangStrategy = new RandomBangStrategy();
+      var shooterGuns = new List<Gun>();
+      var victimGuns = new List<Gun>();
+
+      // Act
+      for (int i = 0; i < 100; i++)
+      {
+        var res = bangStrategy.Bang(1, 1, 1, 1);
+        shooterGuns.Add(res.Item1);
+        victimGuns.Add(res.Item2);
+      }
+
+      // Assert
+      Assert.That(shooterGuns, Is.All.Not.EqualTo(Gun.None));
+    }
+  }
+}
---
+++ Pistol.NET/Pistol.NET/RandomBangStrategy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pistol.NET
+{
+  public class RandomBangStrategy : IBangStrategy
+  {
+    private static readonly Random rnd_ = new Random();
+
+    private static Gun GetRandomGun()
+    {
+      return rnd_.NextBoolean() ? Gun.Right : Gun.Left;
+    }
+
+    public Tuple<Gun, Gun> Bang(int shooterLeftGun, int shooterRightGun, int victimLeftGun, int victimRightGun)
+    {
+      return new Tuple<Gun, Gun>(GetRandomGun(), GetRandomGun());
+    }
+
+    public Gun BangOneOnTwo(int shooterGun, int victimLeftGun, int victimRightGun)
+    {
+      return GetRandomGun();
+    }
+
+    public Gun BangTwoOnOne(int shooterLeftGun, int shooterRightGun, int victimGun)
+    {
+      return GetRandomGun();
+    }
+  }
+}
